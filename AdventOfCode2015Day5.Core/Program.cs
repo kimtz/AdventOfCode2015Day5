8d9a4d4f46a810b0514a93ac9fc40583f70579e5
Kim Tengbom Zetterman <kim.tz@hotmail.com>
2018-02-07 14:12:19 +0100

    Add project files.

---
+++ AdventOfCode2015Day5.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2015Day5.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
